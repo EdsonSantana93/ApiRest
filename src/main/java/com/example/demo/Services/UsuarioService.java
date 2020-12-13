3e2045adab23dc74e57907c4c097f32155550c05
EdsonSantana93 <edson.fs193@gmail.com>
2020-12-13 00:48:48 -0300

    Criação do Service Usuario, integração entre camadas service e controller, melhorias no método buscar todos e criação do buscar usuarios pro id

---
+++ src/main/java/com/example/demo/Services/UsuarioService.java
@@ -0,0 +1,27 @@
+package com.example.demo.Services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.Usuario;
+import com.example.demo.repositories.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+	@Autowired
+	private UsuarioRepository repository;
+
+	public List<Usuario> buscarUsuarios(){
+		return repository.findAll();
+	}
+
+	public Usuario buscarUsuarioId(Long id) {
+		Optional<Usuario> usuario = repository.findById(id);
+		return usuario.get();
+	}
+
+}
--- src/main/java/com/example/demo/controller/UsuarioController.java
+++ src/main/java/com/example/demo/controller/UsuarioController.java
@@ -1,25 +1,34 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.Usuario;
+import com.example.demo.Services.UsuarioService;
 
 @RestController
 @RequestMapping(value = "/usuarios")
 public class UsuarioController {
-
+	
+	@Autowired
+	private UsuarioService service;
+	
 	@GetMapping
 	public ResponseEntity<List<Usuario>> buscarTodosUsuarios() {
-		List<Usuario> u = new ArrayList<>();
-		u.add(new Usuario(null, "Estroncio Silva", "estroncio@gmail.com", "11-99999-9998", "12345"));
-		u.add(new Usuario(null, "Dionesia Pereira", "Dionesia@gmail.com", "11-99999-9999", "12345"));
-		return ResponseEntity.ok().body(u);
+		List<Usuario> usuarios = service.buscarUsuarios();
+		return ResponseEntity.ok().body(usuarios);
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> buscarUsuarioId(@PathVariable Long id){
+		Usuario usuario = service.buscarUsuarioId(id);
+		return ResponseEntity.ok().body(usuario);
+	}		
 
 }
