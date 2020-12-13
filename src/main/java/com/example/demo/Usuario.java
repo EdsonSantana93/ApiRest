76a9959d078c0224322c035a5742a3184a733106
EdsonSantana93 <edson.fs193@gmail.com>
2020-12-12 23:46:47 -0300

    Criação da Entidade e Controller Usuario

---
+++ src/main/java/com/example/demo/Usuario.java
@@ -0,0 +1,68 @@
+package com.example.demo;
+
+import java.io.Serializable;
+
+public class Usuario implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String nome;
+	private String email;
+	private String telefone;
+	private String senha;
+
+	protected Usuario() {
+
+	}
+
+	public Usuario(Integer id, String nome, String email, String telefone, String senha) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.telefone = telefone;
+		this.senha = senha;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}	
+	
+}
---
+++ src/main/java/com/example/demo/controller/UsuarioController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Usuario;
+
+@RestController
+@RequestMapping(value = "/usuarios")
+public class UsuarioController {
+
+	@GetMapping
+	public ResponseEntity<List<Usuario>> buscarTodosUsuarios() {
+		List<Usuario> u = new ArrayList<>();
+		u.add(new Usuario(1, "Estroncio Silva", "estroncio@gmail.com", "11-99999-9998", "12345"));
+		u.add(new Usuario(2, "Dionesia Pereira", "Dionesia@gmail.com", "11-99999-9999", "12345"));
+		return ResponseEntity.ok().body(u);
+	}
+
+}
