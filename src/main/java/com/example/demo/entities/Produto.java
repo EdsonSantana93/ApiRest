e350388bd292aaab8267a4ac56da4b36a4233579
EdsonSantana93 <edson.fs193@gmail.com>
2020-12-15 20:12:49 -0300

    Tabela auxiliar para FK de item produto, e tabela item produto

--- src/main/java/com/example/demo/entities/Produto.java
+++ src/main/java/com/example/demo/entities/Produto.java
@@ -27,8 +27,7 @@ public class Produto implements Serializable {
 	private String UrlImagem;
 
 	@ManyToMany
-	@JoinTable(name = "produto_categoria", joinColumns = @JoinColumn(name="produto_id"),
-	inverseJoinColumns = @JoinColumn(name="categoria_id"))
+	@JoinTable(name = "produto_categoria", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
 	private Set<Categoria> categorias = new HashSet<>();
 
 	public Produto() {
@@ -86,6 +85,31 @@ public class Produto implements Serializable {
 
 	public Set<Categoria> getCategoria() {
 		return categorias;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 
 }
