b4ea01e88fb834ad996866ba212bcfea72df78a5
EdsonSantana93 <edson.fs193@gmail.com>
2020-12-19 19:33:58 -0300

    Associação muitos para muitos entre Produto e Ordem de pedidos

--- src/main/java/com/example/demo/entities/Produto.java
+++ src/main/java/com/example/demo/entities/Produto.java
@@ -12,6 +12,9 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Produto implements Serializable {
@@ -29,7 +32,10 @@ public class Produto implements Serializable {
 	@ManyToMany
 	@JoinTable(name = "produto_categoria", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
 	private Set<Categoria> categorias = new HashSet<>();
-
+	
+	@OneToMany(mappedBy = "id.produto")
+	private Set<ItemPedido> itens = new HashSet<>();
+	
 	public Produto() {
 
 	}
@@ -85,6 +91,15 @@ public class Produto implements Serializable {
 
 	public Set<Categoria> getCategoria() {
 		return categorias;
+	}
+	
+	@JsonIgnore
+	public Set<Pedido> getPedidos(){
+		Set<Pedido> set = new HashSet<>();
+		for (ItemPedido item: itens) {
+			set.add(item.getPedido());
+		}
+		return set;
 	}
 
 	@Override
